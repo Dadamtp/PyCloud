7aa17946aa189decbd027a8a264260d4eb316ff3
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-07-02 08:54:46 +0200

    Encrypt ini file if the user quit addSite function with KeyboardInterrupt

--- PyCloud.py
+++ PyCloud.py
@@ -136,211 +136,221 @@ def validIni(iniFile):
 
 def addSite(decrypt,crypt):
 
-    # Display banner
-    displayBanner()
-    # Get password to decrypt ini gile
-    password = input("[?] Please, enter the password to unlock ini file : ")
-    # Decrypt the ini file to check if the synta is right
-    decryptIni(crypt,decrypt,password)
-    # If the ini file have incorrect syntax, go to menu
-    if not validIni(decrypt):
-        return main()
-    # Else, add a new site to backup
-    else:
 
-        # Initiate config reader
-        configReader = configparser.ConfigParser(delimiters=':')
+    try:
 
-        # Simple error condition with loop to get user answer
-        # Choose folder to backup
-        loop = True
+        # Display banner
         displayBanner()
-        folderBackup = input("[?] Which folder do you want backup : ")
-        while loop:
-            displayBanner()
-            if not os.path.isdir(folderBackup):
-                print(f"[!] The folder {folderBackup} does not exist")
-                folderBackup = input("[?] Which folder do you want backup : ")
-            else:
-                configReader.add_section(folderBackup)
-                loop = False
+        # Get password to decrypt ini gile
+        password = input("[?] Please, enter the password to unlock ini file : ")
+        # Decrypt the ini file to check if the synta is right
+        decryptIni(crypt,decrypt,password)
+        # If the ini file have incorrect syntax, go to menu
+        if not validIni(decrypt):
+            return main()
+        # Else, add a new site to backup
+        else:
 
-        # Choose the targeted server where upload backup
-        displayBanner()
-        site = input("[?] Targeted server to save backup  (hostname or IP) : ")
-        configReader.set(folderBackup,'site',site)
+            # Initiate config reader
+            configReader = configparser.ConfigParser(delimiters=':')
 
-        # Choose the kind of backup
-        # Mirror will make a simple mirror of local folder to a remote folder
-        # Newest will upload only newest and news files
-        loop = True
-        displayBanner()
-        mode = input("[?] What kind of backup do you want to do (mirror/newest) : ")
-        while loop:
-            displayBanner()
-            if mode != 'mirror' and mode != 'newest':
-                print("[!] Please, make choice between (mirror/newest)")
-                mode = input("[?] What kind of backup do you want to do (mirror/newest) : ")
-            else:
-                configReader.set(folderBackup,'mode',mode)
-                loop = False
-
-        # Choose the backup protocol
-        loop = True
-        displayBanner()
-        protocole = input("[?] Which protocole use (ssh/ftp) : ")
-        while loop:
-            displayBanner()
-            if protocole != 'ssh' and protocole != 'ftp':
-                print(f"[!] The {protocole} protocole is not available...")
-                protocole = input("[?] Which protocole use (ssh/ftp) : ")
-            else:
-                configReader.set(folderBackup,'protocole',protocole)
-                loop = False
-
-
-        # Choose the port of the protocol
-        loop = True
-        displayBanner()
-        port = input(f"[?] Which port do you want to use with {protocole} : ")
-        while loop:
-            displayBanner()
-            if int(port) < 1 or int(port) > 65535:
-                print("[!] Please, choose a port between 1 and 65535")
-                port = input(f"[?] Which port do you want to use with {protocole} : ")
-            else:
-                configReader.set(folderBackup,'port',port)
-                loop = False
-
-        #---------------------------------------
-        # If the user choose the FTP protocole |
-        #---------------------------------------
-        if protocole == 'ftp':
-
-            # Want activation SSL/TLS with FTP ?
+            # Simple error condition with loop to get user answer
+            # Choose folder to backup
             loop = True
             displayBanner()
-            ssl = input("[?] Do you want to use SSL/TLS (y/n) : ")
+            folderBackup = input("[?] Which folder do you want backup : ")
             while loop:
                 displayBanner()
-                if ssl != 'y' and ssl != 'n':
-                    print("[!] Please, make choice between (y/n)")
-                    ssl = input("[?] Do you want to use SSL/TLS (y/n) : ")
+                if not os.path.isdir(folderBackup):
+                    print(f"[!] The folder {folderBackup} does not exist")
+                    folderBackup = input("[?] Which folder do you want backup : ")
                 else:
-                    configReader.set(folderBackup,'SSL/TLS',ssl)
+                    configReader.add_section(folderBackup)
                     loop = False
 
-            # Which user to use with FTP ?
+            # Choose the targeted server where upload backup
+            displayBanner()
+            site = input("[?] Targeted server to save backup  (hostname or IP) : ")
+            configReader.set(folderBackup,'site',site)
+
+            # Choose the kind of backup
+            # Mirror will make a simple mirror of local folder to a remote folder
+            # Newest will upload only newest and news files
             loop = True
             displayBanner()
-            user = input("[?] Which user use for FTP login : ")
+            mode = input("[?] What kind of backup do you want to do (mirror/newest) : ")
             while loop:
                 displayBanner()
-                if len(user) == 0:
-                    print("[!] Please, add a non empty username")
-                    user = input("[?] Which user use for FTP login : ")
+                if mode != 'mirror' and mode != 'newest':
+                    print("[!] Please, make choice between (mirror/newest)")
+                    mode = input("[?] What kind of backup do you want to do (mirror/newest) : ")
                 else:
-                    configReader.set(folderBackup,'user',user)
+                    configReader.set(folderBackup,'mode',mode)
                     loop = False
 
-            # Which password ?
-            displayBanner()
-            passwd = input("[?] Which password use for FTP login : ")
-            configReader.set(folderBackup,'password',passwd)
-                    
-        # ---------------------
-        # If protocole is SSH |
-        # ---------------------
-
-        elif protocole == 'ssh':
-
-            # Use a private key ?
+            # Choose the backup protocol
             loop = True
             displayBanner()
-            privateKey = input("[?] Do you want to use a private key with SSH (y/n) : ")
+            protocole = input("[?] Which protocole use (ssh/ftp) : ")
             while loop:
                 displayBanner()
-                if privateKey != 'y' and privateKey != 'n':
+                if protocole != 'ssh' and protocole != 'ftp':
+                    print(f"[!] The {protocole} protocole is not available...")
+                    protocole = input("[?] Which protocole use (ssh/ftp) : ")
+                else:
+                    configReader.set(folderBackup,'protocole',protocole)
+                    loop = False
+
+
+            # Choose the port of the protocol
+            loop = True
+            displayBanner()
+            port = input(f"[?] Which port do you want to use with {protocole} : ")
+            while loop:
+                displayBanner()
+                if int(port) < 1 or int(port) > 65535:
+                    print("[!] Please, choose a port between 1 and 65535")
+                    port = input(f"[?] Which port do you want to use with {protocole} : ")
+                else:
+                    configReader.set(folderBackup,'port',port)
+                    loop = False
+
+            #---------------------------------------
+            # If the user choose the FTP protocole |
+            #---------------------------------------
+            if protocole == 'ftp':
+
+                # Want activation SSL/TLS with FTP ?
+                loop = True
+                displayBanner()
+                ssl = input("[?] Do you want to use SSL/TLS (y/n) : ")
+                while loop:
+                    displayBanner()
+                    if ssl != 'y' and ssl != 'n':
+                        print("[!] Please, make choice between (y/n)")
+                        ssl = input("[?] Do you want to use SSL/TLS (y/n) : ")
+                    else:
+                        configReader.set(folderBackup,'SSL/TLS',ssl)
+                        loop = False
+
+                # Which user to use with FTP ?
+                loop = True
+                displayBanner()
+                user = input("[?] Which user use for FTP login : ")
+                while loop:
+                    displayBanner()
+                    if len(user) == 0:
+                        print("[!] Please, add a non empty username")
+                        user = input("[?] Which user use for FTP login : ")
+                    else:
+                        configReader.set(folderBackup,'user',user)
+                        loop = False
+
+                # Which password ?
+                displayBanner()
+                passwd = input("[?] Which password use for FTP login : ")
+                configReader.set(folderBackup,'password',passwd)
+                        
+            # ---------------------
+            # If protocole is SSH |
+            # ---------------------
+
+            elif protocole == 'ssh':
+
+                # Use a private key ?
+                loop = True
+                displayBanner()
+                privateKey = input("[?] Do you want to use a private key with SSH (y/n) : ")
+                while loop:
+                    displayBanner()
+                    if privateKey != 'y' and privateKey != 'n':
+                        print("[!] Please make a choice between (y/n)")
+                        privateKey = input("[?] Do you want to use a private key with SSH (y/n) : ")
+                    else:
+                        if str(privateKey) == 'y':
+                            loop = True
+                            displayBanner()
+                            keyPath = input("[?] Specify the path of the private key : ")
+                            while loop:
+                                displayBanner()
+                                if not os.path.isfile:
+                                    print(f"[!] The key {keyPath} does not exist")
+                                    keyPath = input("[?] Specify the path of the private key : ")
+                                else:
+                                    loop = False
+                                    configReader.set(folderBackup,'key',keyPath)
+
+                        elif str(privateKey) == 'n':
+
+                            # If don't want to use a private key, which username to login ?
+                            loop = True
+                            displayBanner()
+                            user = input("[?] Which user use for SSH login : ")
+                            while loop:
+                                displayBanner()
+                                if len(user) == 0:
+                                    print("[!] Please, add a non empty username")
+                                    user = input("[?] Which user use for SSH login : ")
+                                else:
+                                    configReader.set(folderBackup,'user',user)
+                                    loop = False
+
+                            # Which password use to login
+                            displayBanner()
+                            passwd = input("[?] Which password use for SSH login : ")
+                            configReader.set(folderBackup,'password',passwd)
+
+            # Add a comment for this backup configuration ?
+            loop = True
+            displayBanner()
+            comment = input("[?] Do you want to add a comment (y/n) : ")
+            while loop:
+                displayBanner()
+                if comment != 'y' and comment != 'n':
                     print("[!] Please make a choice between (y/n)")
-                    privateKey = input("[?] Do you want to use a private key with SSH (y/n) : ")
+                    comment = input("[?] Do you want to add a comment (y/n) : ")
                 else:
-                    if str(privateKey) == 'y':
-                        loop = True
-                        displayBanner()
-                        keyPath = input("[?] Specify the path of the private key : ")
-                        while loop:
-                            displayBanner()
-                            if not os.path.isfile:
-                                print(f"[!] The key {keyPath} does not exist")
-                                keyPath = input("[?] Specify the path of the private key : ")
-                            else:
-                                loop = False
-                                configReader.set(folderBackup,'key',keyPath)
-
-                    elif str(privateKey) == 'n':
-
-                        # If don't want to use a private key, which username to login ?
-                        loop = True
-                        displayBanner()
-                        user = input("[?] Which user use for SSH login : ")
-                        while loop:
-                            displayBanner()
-                            if len(user) == 0:
-                                print("[!] Please, add a non empty username")
-                                user = input("[?] Which user use for SSH login : ")
-                            else:
-                                configReader.set(folderBackup,'user',user)
-                                loop = False
-
-                        # Which password use to login
-                        displayBanner()
-                        passwd = input("[?] Which password use for SSH login : ")
-                        configReader.set(folderBackup,'password',passwd)
-
-        # Add a comment for this backup configuration ?
-        loop = True
-        displayBanner()
-        comment = input("[?] Do you want to add a comment (y/n) : ")
-        while loop:
-            displayBanner()
-            if comment != 'y' and comment != 'n':
-                print("[!] Please make a choice between (y/n)")
-                comment = input("[?] Do you want to add a comment (y/n) : ")
-            else:
-                loop = False
-        if str(comment) == 'y':
-            readComment = input("[?] Add your comment to the backup configuration : \n")
-            configReader.set(folderBackup,'comment',readComment)
+                    loop = False
+            if str(comment) == 'y':
+                readComment = input("[?] Add your comment to the backup configuration : \n")
+                configReader.set(folderBackup,'comment',readComment)
 
 
-        # Make a global view of the configuration
-        loop = True
-        while loop:
-            displayBanner()
-            print(f"[{folderBackup}]")
-            for name,value in configReader.items(folderBackup):
-                print(f"{name} : {value}")
-
-            response = input("\nConfirm backup configuration creation (y/n)")
-            if response != 'y' and response != 'n':
+            # Make a global view of the configuration
+            loop = True
+            while loop:
                 displayBanner()
                 print(f"[{folderBackup}]")
                 for name,value in configReader.items(folderBackup):
                     print(f"{name} : {value}")
+
                 response = input("\nConfirm backup configuration creation (y/n)")
-            else:
-                loop = False
+                if response != 'y' and response != 'n':
+                    displayBanner()
+                    print(f"[{folderBackup}]")
+                    for name,value in configReader.items(folderBackup):
+                        print(f"{name} : {value}")
+                    response = input("\nConfirm backup configuration creation (y/n)")
+                else:
+                    loop = False
 
 
 
-        # Finally write the configuration in the ini file
-        with open(decrypt,'a') as iniFile:
-            configReader.write(iniFile)
+            # Finally write the configuration in the ini file
+            with open(decrypt,'a') as iniFile:
+                configReader.write(iniFile)
 
-        # Encrypt ini file at the end
+            # Encrypt ini file at the end
+            encryptIni(decrypt,crypt,password)
+
+        return
+    
+    except KeyboardInterrupt:
+        displayBanner()
         encryptIni(decrypt,crypt,password)
-
-    return
+        print("\n[+] Successfully encrypted INI file before return to the menu")
+        time.sleep(3)
+        return main()
 
 
 
